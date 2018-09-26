c9589e2cbe047f70c903a8b39e40264f8e121c52
Tom4net <764462478@qq.com>
2018-09-26 21:50:21 +0800

    增加FileServer相关内容

---
+++ src/Core/FileServerCore/Configurations/FileServerConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FileServerCore.Configurations
+{
+    public class FileServerConfiguration
+    {
+        public static FileServerConfiguration Instance
+        {
+            private set;
+            get;
+        }
+
+        private FileServerConfiguration() { }
+
+        public static FileServerConfiguration Create()
+        {
+            Instance = new FileServerConfiguration();
+            return Instance;
+        }
+
+    }
+}
